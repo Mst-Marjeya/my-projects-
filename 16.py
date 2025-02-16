cf2a4e022b4b7bcab0ebb7cb719dd85eb3805d51
Mst-Marjeya <mstmarjeya27@gmail.com>
2025-02-16 19:58:34 +0600

    guessing game

---
+++ 16.py
@@ -0,0 +1,14 @@
+'''sum=0
+number=10
+while number>15:
+    print(sum+number)'''
+
+# number=0
+# while True:
+#     number =+1
+#     if number==3:
+#         continue
+#     if number==5:
+#         break
+#
+# print( "this is the last line")
