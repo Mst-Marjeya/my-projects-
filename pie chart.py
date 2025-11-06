fc2cc89250ec8f852011d710ee29d4bf08ddf244
Mst-Marjeya <mstmarjeya27@gmail.com>
2025-11-07 00:37:49 +0600

    pie chart, histogram , sceteart diaram create
    must remove '#' to see output

---
+++ pie chart.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as mp
+import numpy as np
+x= np.array(['tamim','R','L','s','M'])
+y= np.array([38,92,9,30,65])
+#mp.pie(y)
+
+#mp.hist(x,y)
+#mp.show()
+
+mp.scatter(x,y)
+mp.show()
